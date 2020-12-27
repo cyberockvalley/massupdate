80c30bd4b87ab1c1de28e2fdeb1375e80614ae9e
Feyijinmi Adegoke <cyberockvalley@gmail.com>
2020-12-27 13:43:58 +0100

    ..

--- src/del-state-city.js
+++ src/del-state-city.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 const result = dotenv.config({ path: '.env' })
-
+const fs = require('fs')
+const path = require('path')
 
 var mysql = require('mysql');
 
@@ -11,25 +12,32 @@ var con = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
+var states = []
+
 var run = () => {
     con.connect(function(err) {
         if(err) {
             send(err)
 
         } else {
-            con.query("SELECT id FROM states WHERE country_id=107 limit 5", function (err, result, fields) {
+            con.query("SELECT id FROM states WHERE country_id=107", function (err, result, fields) {
                 if(err) {
                     send(err)
         
                 } else {
-                    var rez = []
                     var i=0
                     while(i < result.length) {
-                        rez.push(result[i].id)
+                        states.push(result[i].id)
                         i++;
                     }
-                    send(rez)
-                    process.exit()
+                    var filePath = path.resolve(__dirname, '../states.json')
+                    saveToFile(filePath, JSON.stringify(states))
+                    send(`A total of ${states.length} states saved to file`)
+                    i=0
+                    while(i < states.length) {
+                        delCitiesThenState(con, states[i], i, states.length - 1)
+                        i++;
+                    }
                 }
             });
         }
@@ -37,12 +45,37 @@ var run = () => {
     })
 }
 
-var deleteCitiesThenState = stateId => {
+var delCitiesThenState = (con, stateId, index, lastIndex) => {
+    con.query(`DELETE FROM cities WHERE state_id=${stateId}`, function (err, result, fields) {
+        if(err) {
+            send({type: "citiesDelete", stateId: stateId, error: err})
+            process.exit()
 
+        } else {
+            con.query(`DELETE FROM states WHERE id=${stateId}`, function (err, result, fields) {
+                if(err) {
+                    send({type: "stateDelete", stateId: stateId, error: err})
+                    process.exit()
+        
+                } else {
+                    send(`State with id ${stateId} deleted with all its cities`)
+                    if(index == lastIndex) {
+                        process.exit()
+                    }
+                }
+            })
+        }
+    });
 }
 
 run();
 
 var send = data => {
     console.log("DATA", data)
+}
+
+var saveToFile = (filePath, data) => {
+    var stream = fs.createWriteStream(filePath, {flags:'a'});
+    stream.write(data)
+    stream.end()
 }
